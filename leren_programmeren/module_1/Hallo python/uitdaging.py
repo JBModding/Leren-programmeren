338ad69d034bdec3d86cfa00eaf2aaae0d3aebbe
JBModding <99064305@mydavinci.nl>
2022-09-08 08:55:56 +0200

    tweede commit

---
+++ leren_programmeren/module_1/Hallo python/uitdaging.py
@@ -0,0 +1,35 @@
+#ingredienten
+
+print('Berekenen van aantal')
+aantalkomkomer = input('voor hoeveel aantal komkomers: ')
+aantalkomkomer = int(aantalkomkomer)
+
+
+mespuntjesperkomkomers = 1
+zeezoutpuntjes = aantalkomkomer * mespuntjesperkomkomers
+
+aantaltenenperkomkomer = 2
+tenen = aantalkomkomer * aantaltenenperkomkomer
+
+aantalgramperkomkomer = 15
+g = aantalkomkomer * aantalgramperkomkomer
+
+aantalyoghurtperkomkomer = 500
+Griekseyoghurt = aantalkomkomer * aantalyoghurtperkomkomer
+
+wijnperkomkmer = 1
+Wittewijnazijn = aantalkomkomer * wijnperkomkmer
+
+olieperkomkomer = 3
+Extraviergeolijfolie = aantalkomkomer * olieperkomkomer
+
+print('mespuntjes zeezout: ', zeezoutpuntjes)
+print('Knoflook: ', tenen)
+print('verse dille: ', g, 'G')
+print('Griekseyoghurt: ', Griekseyoghurt, 'Ml')
+print('Wittewijnazijn: ', Wittewijnazijn, 'el')
+print('Extraviergeolijfolie: ', Extraviergeolijfolie, 'el')
+
+
+# Justin Boom
+# 99064305

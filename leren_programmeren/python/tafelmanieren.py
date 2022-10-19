b07e8ac0e8785bb36b9298590172ffb7082d8e3a
JBModding <99064305@mydavinci.nl>
2022-10-19 09:47:46 +0200

    tafels

--- leren_programmeren/python/tafelmanieren.py
+++ leren_programmeren/python/tafelmanieren.py
@@ -5,3 +5,8 @@ nummer = int(input("welke tafel zou u willen? "))
 for  i in range(1,11):
     som = nummer * i
     print(f"{nummer} x {i} = {som}")
+
+
+
+
+    # https://github.com/JBModding/Leren-programmeren.git
